bfd6620cac0fdb2f06da3a7002d5e8243a4dc3d7
IShkorubsky <illyashkorubsky@hotmail.com>
2023-06-25 12:00:34 +0100

    making the movement feel more arcady

--- Car Physics Mastered/Assets/Scripts/CarController.cs
+++ Car Physics Mastered/Assets/Scripts/CarController.cs
@@ -1,18 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string HORIZONTAl = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _currentSteerAngle;
+    private float _currentBreakForce;
+    private bool _isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+    
+    [SerializeField] private MeshRenderer frontLeftWheelTransform;
+    [SerializeField] private MeshRenderer frontRightWheelTransform;
+    [SerializeField] private MeshRenderer rearLeftWheelTransform;
+    [SerializeField] private MeshRenderer rearRightWheelTransform;
+
+    private void FixedUpdate()
     {
-        
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
-        
+        _horizontalInput = Input.GetAxis(HORIZONTAl);
+        _verticalInput = Input.GetAxis(VERTICAL);
+
+        if (_verticalInput == 0 || Input.GetKey(KeyCode.Space))
+        {
+            _isBreaking = true;
+        }
+        else
+        {
+            _isBreaking = false;
+        }
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = _verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = _verticalInput * motorForce;
+        rearLeftWheelCollider.motorTorque = _verticalInput * motorForce;
+        rearRightWheelCollider.motorTorque = _verticalInput * motorForce;
+        _currentBreakForce = _isBreaking ? breakForce : 0f;
+        ApplyBreaking();
+
+    }
+
+    private void ApplyBreaking()
+    {
+        frontLeftWheelCollider.brakeTorque = _currentBreakForce;
+        frontRightWheelCollider.brakeTorque = _currentBreakForce;
+        rearLeftWheelCollider.brakeTorque = _currentBreakForce;
+        rearRightWheelCollider.brakeTorque = _currentBreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        _currentSteerAngle = maxSteerAngle * _horizontalInput;
+        frontLeftWheelCollider.steerAngle = _currentSteerAngle;
+        frontRightWheelCollider.steerAngle = _currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, MeshRenderer wheelMeshRenderer)
+    {
+        wheelCollider.GetWorldPose(out var position,out var rotation);
+        wheelMeshRenderer.transform.position = position;
+        wheelMeshRenderer.transform.rotation = rotation;
     }
 }
